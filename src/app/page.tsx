3361f6cefa34b6c3e80be66cc5a06932ae188569
LucaSs55 <lucassantos2018d@gmail.com>
2026-05-02 14:33:16 +0000

    feat:Add stack screen to SPA aplication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Hero } from "../components/hero/Hero";
 import About from "./about/page";
 import { Footer } from "@/components/footer/Footer";
+import { Stack } from "./stacks/page";
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
         <section id="sobre">
           <About />
         </section>
-        <section id="stack">{/* Stack component */}</section>
+        <section id="stack">
+          <Stack />
+        </section>
         <section id="projetos">{/* Projects component */}</section>
         <section>
           <Footer />
